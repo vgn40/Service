1e452a8899410cd7f30258e2eff78d052a6dfd25
vgn40 <victornylev@hotmail.dk>
2024-12-11 11:28:45 +0100

    added

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from db import init_db
+import inspect
 
 load_dotenv()
 
@@ -17,7 +18,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 app.config['JWT_SECRET_KEY'] = SECRET_KEY
 jwt = JWTManager(app)
 
-# comment qweqwe
 swagger_config = {
     "headers": [],
     "specs": [
@@ -291,6 +291,45 @@ def delete_service(service_id):
     
     return jsonify({"message": "Service deleted successfully"}), 200
 
+
+@app.route('/endpoints', methods=['GET'])
+def endpoints():
+    """
+    List all available endpoints in the API, including their descriptions, methods, and JWT token requirements.
+    --- 
+    tags:
+      - Utility
+    responses:
+      200:
+        description: A list of all available routes with their descriptions, methods, and JWT token requirements.
+    """
+    excluded_endpoints = {'static', 'flasgger.static', 'flasgger.oauth_redirect', 'flasgger.<lambda>', 'flasgger.apispec'}
+    excluded_methods = {'HEAD', 'OPTIONS'}
+    routes = []
+
+    for rule in app.url_map.iter_rules():
+        if rule.endpoint not in excluded_endpoints:
+            func = app.view_functions.get(rule.endpoint)
+            if not func:
+                continue
+
+            # Get the docstring
+            full_docstring = inspect.getdoc(func)
+            docstring = full_docstring.split('---')[0].replace("\n", " ").strip() if full_docstring else None
+
+            # Check if the @jwt_required() decorator is applied
+            jwt_required = "@jwt_required" in inspect.getsource(func).split('\n')[1]
+
+            # Exclude methods
+            methods = list(rule.methods - excluded_methods)
+
+            routes.append({
+                'endpoint': rule.rule,
+                'methods': methods,
+                'description': docstring,
+                'jwt_required': jwt_required
+            })
+    return jsonify({'endpoints': routes}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
